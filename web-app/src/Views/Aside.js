b49a3aa5a042d0e218ba9d59a82b7b6ddc0a0133
Lucas Bonnaire <lbonnairepro@gmail.com>
2023-12-15 18:15:36 +0100

    ajout d'un bouton "checked" et des fonctionnalités de check et d'épinglage

--- web-app/src/Views/Aside.js
+++ web-app/src/Views/Aside.js
@@ -47,8 +47,40 @@ function Aside({ notes, fetchNotes, apiErrorToast }){
         );
     }
 
-    function pinNote(id){
-        console.log("Note épinglée");
+    async function pinNote(id){
+        const note = notes.find(note => note.id === id);
+        try{
+            {note.pinned ? note.pinned = false : note.pinned = true}
+            const response = await fetch('/notes/'+id, {
+                method: "PUT",
+                body: JSON.stringify(note),
+                headers: {
+                    "Content-Type": "application/json"
+                },
+            });
+        }
+        catch(e){
+            console.error("Erreur à la modification du statut 'épinglé' de la note - "+e);
+            apiErrorToast();
+        }
+    }
+
+    async function checkNote(id){
+        const note = notes.find(note => note.id === id);
+        try{
+            {note.checked ? note.checked = false : note.checked = true}
+            const response = await fetch('/notes/'+id, {
+                method: "PUT",
+                body: JSON.stringify(note),
+                headers: {
+                    "Content-Type": "application/json"
+                },
+            });
+        }
+        catch(e){
+            console.error("Erreur à la modification du statut 'faite' de la note - "+e);
+            apiErrorToast();
+        }
     }
 
     return(
@@ -64,6 +96,7 @@ function Aside({ notes, fetchNotes, apiErrorToast }){
                     </div>
                     <ol className='Note-list'>
                         {notes
+                            .filter(note => note.pinned)
                             .filter(note => {
                                 if(searchQuery === ""){
                                     return note;
@@ -71,6 +104,15 @@ function Aside({ notes, fetchNotes, apiErrorToast }){
                                     return note;
                                 }else if(note.content.toLowerCase().includes(searchQuery.toLowerCase())){
                                     return note;
+                                }
+                            })
+                            .sort((a, b) => {
+                                if (a.pinned && !b.pinned) {
+                                    return -1;
+                                } else if (!a.pinned && b.pinned) {
+                                    return 1;
+                                } else {
+                                    return 0;
                                 }
                             })
                             .map((note) => (
@@ -81,10 +123,63 @@ function Aside({ notes, fetchNotes, apiErrorToast }){
                                             <div className='Note-link-content'>{highlightSearchTerms(note.content.substring(0,20), searchQuery)}{note.content.length>=20 ? "..." : null}</div>
                                         </div>
                                         <div>
-                                            <button className='Note-Pin-Button' onClick={pinNote(note.id)}>
+                                            <button className='Note-Button' onClick={() => {pinNote(note.id)}}>
                                                 <img 
                                                     src={note.pinned ? "/images/pinned_white.png" : "/images/pin_white.png"}
                                                     alt={note.pinned ? "Pinned" : "Not pinned"}
+                                                />
+                                            </button>
+                                            <button className='Note-Button' onClick={() => {checkNote(note.id)}}>
+                                                <img 
+                                                    src={note.checked ? "/images/checked.png" : "/images/check.png"}
+                                                    alt={note.checked ? "Checked" : "Not checked"}
+                                                />
+                                            </button>
+                                        </div>
+                                    </Link>
+                                </li>
+                            ))
+                        }
+                        <hr className='Side-separator'></hr>
+                        {notes
+                            .filter(note => !note.pinned)
+                            .filter(note => {
+                                if(searchQuery === ""){
+                                    return note;
+                                }else if(note.title.toLowerCase().includes(searchQuery.toLowerCase())){
+                                    return note;
+                                }else if(note.content.toLowerCase().includes(searchQuery.toLowerCase())){
+                                    return note;
+                                }
+                            })
+                            .sort((a, b) => {
+                                if (a.pinned && !b.pinned) {
+                                    return -1; // Note a épinglée, b non épinglée (a vient avant b)
+                                } else if (!a.pinned && b.pinned) {
+                                    return 1; // Note b épinglée, a non épinglée (b vient avant a)
+                                } else {
+                                    // Notes épinglées et non épinglées restent dans l'ordre actuel
+                                    return 0;
+                                }
+                            })
+                            .map((note) => (
+                                <li key={note.id}>
+                                    <Link to={"/notes/"+note.id} className={note.id === parseInt(currentID) ? 'Note-link active' : 'Note-link'}>
+                                        <div className='Note-link-container'>
+                                            <div>{highlightSearchTerms(note.title.substring(0,30), searchQuery)}{note.title.length>=30 ? '...' : null}</div>
+                                            <div className='Note-link-content'>{highlightSearchTerms(note.content.substring(0,20), searchQuery)}{note.content.length>=20 ? "..." : null}</div>
+                                        </div>
+                                        <div className='Note-Buttons-Container'>
+                                            <button className='Note-Button' onClick={() => {pinNote(note.id)}}>
+                                                <img 
+                                                    src={note.pinned ? "/images/pinned_white.png" : "/images/pin_white.png"}
+                                                    alt={note.pinned ? "Pinned" : "Not pinned"}
+                                                />
+                                            </button>
+                                            <button className='Note-Button' onClick={() => {checkNote(note.id)}}>
+                                                <img 
+                                                    src={note.checked ? "/images/checked.png" : "/images/check.png"}
+                                                    alt={note.checked ? "Checked" : "Not checked"}
                                                 />
                                             </button>
                                         </div>
